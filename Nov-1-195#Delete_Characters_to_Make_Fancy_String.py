479340492fa094b40478f25b2496d358865c56f6
soul <j983581@gmail.com>
2024-11-01 11:42:50 +0530

    just started

---
+++ Nov-1-195#Delete_Characters_to_Make_Fancy_String.py
@@ -0,0 +1,11 @@
+# 1957. Delete Characters to Make Fancy String
+
+def fancyString(stri):
+    left=right = stri[0]
+    i=0
+    while 
+    return stri
+            
+        
+    
+print(fancyString("aaaabaaaa"))
