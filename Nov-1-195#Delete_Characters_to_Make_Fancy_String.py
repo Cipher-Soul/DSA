6a5147037d444604e4bb0c91a53c505169a2e91f
soul <j983581@gmail.com>
2024-11-01 12:15:31 +0530

    completed

--- Nov-1-195#Delete_Characters_to_Make_Fancy_String.py
+++ Nov-1-195#Delete_Characters_to_Make_Fancy_String.py
@@ -1,11 +1,17 @@
 # 1957. Delete Characters to Make Fancy String
 
 def fancyString(stri):
-    left=right = stri[0]
+    final  = ""
     i=0
-    while 
-    return stri
-            
+    for s in stri:
+        i+=1
+        # print('=>',i)
+        if len(final)>=2 and final[-1] ==s and final[-2] == s:
+            pass           
+        else:
+            final += s
+            # print(final)
         
+    return final 
     
 print(fancyString("aaaabaaaa"))
